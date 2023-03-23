d71206fd19f09ee86e23681935bd922e8a4cd753
rassi0429 <taitokokoa+rassi@gmail.com>
2023-03-23 21:15:49 +0900

    feat: hostname

--- main.mjs
+++ main.mjs
@@ -1,8 +1,9 @@
 import express from "express";
+import os from "os"
 const app = express()
 
 app.listen(3000, () => console.log("API OK!"))
 app.get("/", (req, res) => {
     console.log("GET Request!")
-    res.send("Hello! this is Express! Today is " + new Date())
+    res.send("Hello! this is Express! Today is " + new Date() + " hostname is " + os.hostname())
 })
